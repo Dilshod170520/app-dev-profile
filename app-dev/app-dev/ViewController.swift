2e70472feffcf1cc017f267c48140de7e00b79aa
Dilshod Qulmirzayev <dilshodqulmirzayev21@gmail.com>
2023-09-02 11:49:36 +0500

    innetiol xcode project

---
+++ app-dev/app-dev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app-dev
+//
+//  Created by MacBook Pro on 02/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
